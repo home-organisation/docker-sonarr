fb7a6390aa02bc6834f0397fa2e47bb2cbeec6d1
bizalu <chprok@gmail.com>
2023-05-16 14:52:10 +0200

    Update 99-restart.py

--- custom-script/99-restart.py
+++ custom-script/99-restart.py
@@ -7,7 +7,6 @@ import logging
 ###########################################################
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-
 ###########################################################
 # DEFINE FUNCTION
 ###########################################################
