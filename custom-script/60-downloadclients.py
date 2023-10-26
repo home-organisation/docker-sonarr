b06a1f6b23116eb398c9ec728292d6569cc679b9
bizalu <chprok@gmail.com>
2023-10-26 21:42:47 +0200

    add category to download client (optional)

--- custom-script/60-downloadclients.py
+++ custom-script/60-downloadclients.py
@@ -15,10 +15,11 @@ SONARR_DB = '/config/sonarr.db'
 ###########################################################
 # DEFINE FUNCTION
 ###########################################################
-def set_downloadclients(database, name, url, port, username, password):
+def set_downloadclients(database, name, url, port, username, password, category):
     # Create download clients in database
     data = ("1", name, "Transmission", '{"host": "' + url + '", "port": "' + port
-            + '", "useSsl": "false", "urlBase": "/transmission/", "username": "' + username + '", "password": "' + password + '"}',
+            + '", "useSsl": "false", "urlBase": "/transmission/", "username": "' + username + '", \
+            "password": "' + password + '", "tvCategory": "' + category +'"}',
             "TransmissionSettings", "1", "1", "1")
     query = "INSERT INTO DownloadClients (Enable,Name,Implementation,Settings,ConfigContract,Priority," \
             "RemoveCompletedDownloads,RemoveFailedDownloads) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
@@ -36,7 +37,7 @@ def set_downloadclients(database, name, url, port, username, password):
         logging.error('SQLite error: %s' % (' '.join(er.args)))
         return None
     else:
-        return {"username": username, "password": password, "url": url, "port": port}
+        return {"username": username, "password": password, "url": url, "port": port, "category": category}
 
 
 def get_downloadclients(database, name):
@@ -65,12 +66,13 @@ def get_downloadclients(database, name):
         password = json.loads(rows[0][0])["password"]
         port = json.loads(rows[0][0])["port"]
         url = json.loads(rows[0][0])["host"]
-        return {"username": username, "password": password, "url": url, "port": port}
+        category = json.loads(rows[0][0])["tvCategory"]
+        return {"username": username, "password": password, "url": url, "port": port, "category": category}
 
 
-def update_downloadclients(database, name, url, port, username, password):
+def update_downloadclients(database, name, url, port, username, password, category):
     data = ('{"host": "' + url + '", "port": "' + port + '", "useSsl": "false", "urlBase": "/transmission/", "username": "'
-            + username + '", "password": "' + password + '"}', name)
+            + username + '", "password": "' + password + '", "tvCategory": "' + category +'"}', name)
     query = "UPDATE DownloadClients SET Settings = ? WHERE Name = ?"
 
     connexion = sqlite3.connect(database)
@@ -86,7 +88,7 @@ def update_downloadclients(database, name, url, port, username, password):
         logging.error('SQLite error: %s' % (' '.join(er.args)))
         return None
     else:
-        return {"username": username, "password": password, "url": url, "port": port}
+        return {"username": username, "password": password, "url": url, "port": port, "category": category}
 
 
 ###########################################################
@@ -98,6 +100,9 @@ if __name__ == '__main__':
     DOWNLOAD_PASSWORD = os.environ.get('DOWNLOAD_PASSWORD')
     DOWNLOAD_URL = os.environ.get('DOWNLOAD_URL')
     DOWNLOAD_PORT = os.environ.get('DOWNLOAD_PORT')
+    DOWNLOAD_CATEGORY = os.environ.get('DOWNLOAD_TVCATEGORY')
+    if DOWNLOAD_CATEGORY is None:
+        DOWNLOAD_CATEGORY = ""
     if DOWNLOAD_USER is None or DOWNLOAD_PASSWORD is None or DOWNLOAD_NAME is None or DOWNLOAD_URL is None or DOWNLOAD_PORT is None:
         logging.warning("DOWNLOAD_NAME, DOWNLOAD_USER, DOWNLOAD_PASSWORD, DOWNLOAD_URL or DOWNLOAD_PORT with no value, nothing to do")
         sys.exit(0)
@@ -107,13 +112,13 @@ if __name__ == '__main__':
     if CLIENT is None:
         sys.exit(1)
 
-    if CLIENT["username"] == "" and CLIENT["password"] == "" and CLIENT["port"] == "" and CLIENT["url"] == "":
-        CLIENT = set_downloadclients(SONARR_DB, DOWNLOAD_NAME, DOWNLOAD_URL, DOWNLOAD_PORT, DOWNLOAD_USER, DOWNLOAD_PASSWORD)
+    if CLIENT["username"] == "" and CLIENT["password"] == "" and CLIENT["port"] == "" and CLIENT["url"] == "" and CLIENT["category"] == "":
+        CLIENT = set_downloadclients(SONARR_DB, DOWNLOAD_NAME, DOWNLOAD_URL, DOWNLOAD_PORT, DOWNLOAD_USER, DOWNLOAD_PASSWORD, DOWNLOAD_CATEGORY)
         if CLIENT is None:
             sys.exit(1)
 
-    if CLIENT["username"] != DOWNLOAD_USER or CLIENT["password"] != DOWNLOAD_PASSWORD or CLIENT["port"] != DOWNLOAD_PORT or CLIENT["url"] != DOWNLOAD_URL:
+    if CLIENT["username"] != DOWNLOAD_USER or CLIENT["password"] != DOWNLOAD_PASSWORD or CLIENT["port"] != DOWNLOAD_PORT or CLIENT["url"] != DOWNLOAD_URL or CLIENT["category"] != DOWNLOAD_CATEGORY:
         logging.info("Download Client %s for downloader %s already exist but with an other parameters, update ..." % (DOWNLOAD_NAME, DOWNLOAD_URL))
-        CLIENT = update_downloadclients(SONARR_DB, DOWNLOAD_NAME, DOWNLOAD_URL, DOWNLOAD_PORT, DOWNLOAD_USER, DOWNLOAD_PASSWORD)
+        CLIENT = update_downloadclients(SONARR_DB, DOWNLOAD_NAME, DOWNLOAD_URL, DOWNLOAD_PORT, DOWNLOAD_USER, DOWNLOAD_PASSWORD, DOWNLOAD_CATEGORY)
         if CLIENT is None:
             sys.exit(1)
